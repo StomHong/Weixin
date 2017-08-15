f8fe64b06e7c9b3f4f2bb3b2ed7fd56ef929926d
stomhong <stomhong@163.com>
2017-08-16 00:03:08 +0800

    用kotlin改写实体类

---
+++ app/src/main/java/com/stomhong/weixin/data/local/vo/WeixinInfo.kt
@@ -0,0 +1,30 @@
+package com.stomhong.weixin.data.local.vo
+
+/**
+ * @author StomHong
+ * *
+ * @version 1.0.0
+ * *
+ * @since 1.0.0
+ * * Date: 2017/7/26
+ * * Description:
+ */
+internal class WeixinInfo {
+    /**
+     * 消息头像
+     */
+    val face: String = ""
+    /**
+     * 标题
+     */
+    val title: String = ""
+    /**
+     * 消息内容
+     */
+    val content: String = ""
+    /**
+     * 间隔时间
+     */
+    val spacingTime: String = ""
+    //    private String
+}
